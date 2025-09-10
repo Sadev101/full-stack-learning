d46397dc7327e5ca4059566a5c859ec465036b4e
thiraphong <thiraphongkuwijitjaru@gmail.com>
2025-09-10 23:49:30 +0700

    login api

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -9,27 +9,57 @@ const registerUser = catchAsync(async (req, res) => {
   if (!email || !name || !password)
     return res.status(400).json({ message: "failed" });
 
-  const user = prisma.user.findUnique({
-    where: { name },
+  const user = await prisma.user.findUnique({
+    where: { email },
   });
   if (user) {
-    const hashedPassword = await bcrypt.hash(
-      password,
-      10,
-      process.env.BCRYPT_SECRET
-    );
-    const token = jwt.sign({ email, name }, process.env.JWT_SECRET, {
-      expiresIn: "24h",
-    });
-
-    req.user = { email, name };
+    return res.status(409).json({ message: "User already exists" });
   }
 
-  res.status(201).json({ message: "success" });
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await prisma.user.create({
+    data: { name, email, password: hashedPassword },
+  });
+
+  const token = jwt.sign(
+    { id: newUser.id, role: newUser.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+
+  res.status(201).json({
+    message: "success",
+    user: { id: newUser.id, email: newUser.email, name: newUser.name },
+    token,
+  });
 });
 
-const loginUser = (req, res) => {
-  res.status(200).json({ message: "success" });
-};
+const loginUser = catchAsync(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(400).json({ message: "fail" });
+
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) return res.status(404).json({ message: "fail" });
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+  if (!isPasswordCorrect) return res.status(404).json({ message: "fail" });
+
+  const token = jwt.sign(
+    { id: user.id, role: user.role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "24h",
+    }
+  );
+
+  res.status(200).json({
+    message: "success",
+    user: { id: user.id, email: user.email, name: user.name },
+    token,
+  });
+});
 
 module.exports = { registerUser, loginUser };
